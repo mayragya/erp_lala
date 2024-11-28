973d9c4fb3cf40129ee69eeff3d21c86f1a60f0b
Mayra Yépez <123526533+mayragya@users.noreply.github.com>
2024-11-27 23:26:49 -0600

    cambios para docker

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 //jalar la cookie de sesión 
 session_start(); 
 //llamado de scripts de conexión con la base de datos y limpieza de datos
-include_once 'config.php'; 
-include_once 'regex.php';  
+include ("./php/config.php"); 
+include ("./php/regex.php");
 
 // Verificar conexión con la base de datos 
 $conn = db_user(); 
@@ -40,7 +40,7 @@ $serverName = gethostname();
          <img src="../images/VC3.jpg" class="form-img"  loading="lazy"> 
 
          
-    <form action="index.php" method="POST" class="login-form">
+    <form action="php/login.php" method="POST" class="login-form">
     <label for="userName" class="label-login">Usuario</label>
     <br>
         <input type="text" class="input-login" name="userName" required>
@@ -61,43 +61,3 @@ $serverName = gethostname();
     <script src="../js/js.js"></script>
 </body>
 </html>
-<?php
-
-//codigo de login por parte del backend 
-// Procesar el formulario solo si el método de la solicitud es POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $userName = $_POST['userName'] ?? ''; 
-    $userName = cleanEmail($userName); 
-    $pswd = $_POST['password'] ?? ''; 
-    $pswd = cleanPass($pswd); 
-
-    // Verificación con la base de datos utilizando Prepared Statements
-    $stmt = $conn->prepare("SELECT * FROM users WHERE userName = ? AND password = ?");
-    $stmt->bind_param("ss", $userName, $pswd);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        // Credenciales correctas
-        $user = $result->fetch_assoc();
-        $_SESSION['user_id'] = $user['id']; 
-        $_SESSION['status'] = 'Conectado';
-        session_regenerate_id(true);
-        setcookie("isUserLoggedIn", "true", time() + 3600, "/"); // La cookie expira en 1 hora
-        header("Location: principal.php");
-        exit(); 
-    } else {
-        // Credenciales incorrectas
-        echo "<script>
-        alert('Datos incorrectos, favor de verificar correo o contraseña');
-        window.location = 'index.php';
-        </script>";
-        $_SESSION['status'] = 'Desconectado';
-
-    }
-
-    $stmt->close();
-}
-
-$conn->close();
-?>
